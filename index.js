be53f7f9467c9d044d3005667f9739b371160417
Jonathan <jonathan.fetterolf@gmail.com>
2023-08-01 09:49:37 -0400

    all tests complete

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ document.getElementById('main').remove();
 
 console.log(document.getElementsByTagName('body'));
 
-console.log(document.getElementById(".body"))
+console.log('body:', document.querySelector('body'))
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Chloe is the champion'
+
+document.querySelector('body').append(newHeader);
