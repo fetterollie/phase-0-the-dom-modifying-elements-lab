065c8554a98fdc92114c42d8575be47c1dd41ac1
Jonathan <jonathan.fetterolf@gmail.com>
2023-07-31 22:15:08 -0400

    fixed a test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// console.log(document.getElementById('main'));
+
+// document.getElementById('body').removeChild();
+
+document.getElementById('main').remove();
+
+console.log(document.getElementsByTagName('body'));
+
+console.log(document.getElementById(".body"))
